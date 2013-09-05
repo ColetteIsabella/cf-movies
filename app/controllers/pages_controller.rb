e331c769a34e69bd1e68ae870fb23c3fde093f6d
ColetteIsabella <coletteisabella@gmail.com>
2013-09-05 13:50:12 -0700

    Added paragraphs and my favorite movie

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,6 +3,8 @@ class PagesController < ApplicationController
   def home
     @first_movie = Movie.first
     @vote_number = @first_movie.votes.count
+
+    @best_movie = Movie.find_by title: "Upstream Color"
   end
 
 end
